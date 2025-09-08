ae7fcda6204afed8b77bd539185babb6c4cbd43e
EaeDave <d4vd.sv@gmail.com>
2025-09-07 21:58:42 -0300

    feat: typescript is javascript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-console.log('Hello, World!');
+// typescript é transpilado para javascript
+// é vantajoso para o dev, pois ajuda a identificar possíveis erros e tem melhor ajuda de autocomplete devido sua tipagem opcional estática
